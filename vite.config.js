4829519efe941122c27f9e5d22b06dcf5993e931
Cuculici Iulian <iuliancuculici@gmail.com>
2024-09-22 12:00:57 +0300

    restart

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/snowboard-calculator/', // Înlocuiește cu numele repository-ului tău GitHub
-  plugins: [react()],
-});
